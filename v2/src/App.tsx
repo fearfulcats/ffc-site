e375ca009990013342131435de5b5d47994edde6
Kitteh <akilidayne@gmail.com>
2023-10-16 05:49:10 -0500

    added some story (blog posts and messages)

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 type Message = {
   content: string,
-  author: "Rozebur" | "Cozy_Cat" | "SoulSella" | "Kitari",
+  author: "Rozebur" | "Cozy_Cat" | "SoulSella" | "KeytarE" | "MaxIsWax",
   server: boolean,
   connected: boolean
 }
@@ -37,7 +37,7 @@ const store = createMutable({
       },
       {
         author: "Cozy_Cat",
-        content: "oh hey roze, i was thinking about the question you posed last time i was online. it was quite interesting.",
+        content: "oh hey roze, good timing. i was just thinking about the question you posed last time i was online. it was quite interesting.",
         connected: true,
         server: false
       },
@@ -100,13 +100,163 @@ const store = createMutable({
         content: "you know he hates you, right?",
         connected: true,
         server: false
-      }
+      },
+      {
+        author: "SoulSella",
+        content: "What's his problem with me? I really don't get it.",
+        connected: true,
+        server: false
+      },
+      {
+        author: "Cozy_Cat",
+        content: "that's just the kind of person he is. it's not easy to get on his good side if he's judged you to be of an incompatible personality type.",
+        connected: true,
+        server: false
+      },
+      {
+        author: "SoulSella",
+        content: "Whatever. It's his own problem then. I mean, I'm a perfectly fine guy!",
+        connected: true,
+        server: false
+      },
+      {
+        author: "Cozy_Cat",
+        content: "to each their own!",
+        connected: true,
+        server: false
+      },
+      {
+        author: "SoulSella",
+        content: "What's that supposed to mean huh? That's rhetorical by the way. I can see the shade you're throwing toward me.",
+        connected: true,
+        server: false
+      },
+      {
+        author: "Cozy_Cat",
+        content: "i'm aware. and i'm also joking ehehehe ^_^'",
+        connected: true,
+        server: false
+      },
+      {
+        author: "Cozy_Cat",
+        content: "anywaysssss... i've gotta head out for a bit. i'll talk to you guys later today!",
+        connected: true,
+        server: false
+      },
+      {
+        author: "SoulSella",
+        content: "See you around.",
+        connected: true,
+        server: false
+      },
+      {
+        author: "Cozy_Cat",
+        content: "null",
+        connected: false,
+        server: true
+      },
+      {
+        author: "MaxIsWax",
+        content: "AWWWWW I JUST MISSED CC?? that SUCKS! :C",
+        connected: true,
+        server: false
+      },
+      {
+        author: "SoulSella",
+        content: "Oh dang, you actually missed him by like a minute.",
+        connected: true,
+        server: false
+      },
+      {
+        author: "MaxIsWax",
+        content: "I STEPPED AWAY FROM MY COMPUTER FOR LIKE. ONE SECOND. >:CCCC",
+        connected: true,
+        server: false
+      },
+      {
+        author: "SoulSella",
+        content: "I've been connected for like 5 minutes, you can't say it's only been one second.",
+        connected: true,
+        server: false
+      },
+      {
+        author: "MaxIsWax",
+        content: "IT'S CALLED EXAGGERATIONNNNNNNNNNNNNN!!!!!!!!!!!!!!!!!",
+        connected: true,
+        server: false
+      },
+      {
+        author: "SoulSella",
+        content: "...I think I feel the same way about you as Roze does about me.",
+        connected: true,
+        server: false
+      },
+      {
+        author: "MaxIsWax",
+        content: "YOURE SO MEAN :(",
+        connected: true,
+        server: false
+      },
+      {
+        author: "SoulSella",
+        content: "I'm just messing with y- wait, how do you know how Roze feels about me?",
+        connected: true,
+        server: false
+      },
+      {
+        author: "MaxIsWax",
+        content: "ummm doesnt EVERYONE??",
+        connected: true,
+        server: false
+      },
+      {
+        author: "SoulSella",
+        content: "Am I the only one who thought we were on good terms?",
+        connected: true,
+        server: false
+      },
+      {
+        author: "MaxIsWax",
+        content: "ITS POSSIBLE!",
+        connected: true,
+        server: false
+      },
+      {
+        author: "SoulSella",
+        content: "Great. Good to hear. I'm gonna go now.",
+        connected: true,
+        server: false
+      },
+      {
+        author: "MaxIsWax",
+        content: ":(",
+        connected: true,
+        server: false
+      },
+      {
+        author: "SoulSella",
+        content: "null",
+        connected: false,
+        server: true
+      },
+      {
+        author: "MaxIsWax",
+        content: "IM ALONEEEE AGAINNNNNNNNNNNN",
+        connected: true,
+        server: false
+      },
+      {
+        author: "MaxIsWax",
+        content: "null",
+        connected: false,
+        server: true
+      },
     ] as Message[]
   },
   history: {
     messageList1: [
       {
-        author: "Kitari",
+        author: "KeytarE",
         content: "null",
         connected: true,
         server: true
@@ -142,6 +292,42 @@ const store = createMutable({
       content: "when are they adding an option to delete blogs omg",
       date: "24/04/XX"
     },
+    {
+      content: "gonna be inactive for a bit, my parents have been really annoying lately. sorry :/",
+      date: "25/04/XX"
+    },
+    {
+      content: "i have some good news for everyone on cat comms!!! its a secret tho so i can't say anything about it yet, just hyping you all up :3",
+      date: "30/04/XX"
+    },
+    {
+      content: "everything's getting finalized so you'll be hearing about it pretty soon!",
+      date: "02/05/XX"
+    },
+    {
+      content: "i'm a bit nervous about it now... idk i'm just getting really anxious about everything...",
+      date: "03/05/XX"
+    },
+    {
+      content: "sometimes i wonder if i'll ever change",
+      date: "04/05/XX"
+    },
+    {
+      content: "well, whatever. i guess i don't need to change. i'm fine with the person i am. i have to be",
+      date: "05/05/XX"
+    },
+    {
+      content: "sorry for being inactive in the chatroom. i've just got so much on my plate lately. the news is still coming though, in a couple more days maybe",
+      date: "06/05/XX"
+    },
+    {
+      content: "ok ill be allowed to talk about it on the 10th",
+      date: "07/05/XX"
+    },
+    {
+      content: "i really hope you all will be happy with what you're going to be hearing",
+      date: "08/05/XX"
+    },
 
   ] as Blog[]
 })
@@ -176,10 +362,10 @@ const App: Component = () => {
   return (
     <>
       <div class="screensaver">
-        <p class="connection">You are currently connected to Extended Area Network 34B "Cat Communications" on July 30, 20XX.</p>
+        <p class="connection">You are currently connected to Extended Area Network 34B "Cat Communications" on May 9th, 20XX.</p>
         <div style="display: grid; justify-content: center; padding: 10vh 2vw">
           <div style="place-self: start; text-align: center; height: 158px; width: 158px;" class="box">
-            <div style="padding-top: 15px;">
+            <div style="padding-top: 8px;">
               fearful<br /><br />cats<br /><br />live<br /><br />here
             </div>
           </div>
@@ -232,19 +418,21 @@ function Console() {
       <div>
 
 
-        <div class="console" style=" padding-top: 5vh; padding-bottom: 1vh">
+        <div class="console" style=" padding-top: 5vh;">
           <Show
             when={!store.messagesRead}
             children={
               <h1 class="newMessages">You have <i>{store.messages.messageList1.length}</i> unread messages!</h1>
             }
           />
-
+          
           <For
             each={store.messages.messageList1}
             children={(message) => <MessageDecoder message={message} />}
           />
-
+          <div class='connected'>
+            Users currently connected: 0
+          </div>
         </div>
         <div style={"display: flex"}>
           <Show
@@ -290,8 +478,10 @@ const MessageDecoder = (props: { message: Message }) => {
         return (<p class="speakerRed" textContent={"> " + message.content} />)
       case 'SoulSella':
         return (<p class="speakerYellow" textContent={"> " + message.content} />)
-      case 'Kitari':
+      case 'KeytarE':
         return (<p class="speakerLime" textContent={"> " + message.content} />)
+      case 'MaxIsWax':
+        return (<p class="speakerGreen" textContent={"> " + message.content} />)
       default:
         return (<p textContent={"error"} />)
     }
